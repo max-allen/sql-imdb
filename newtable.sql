cec15a59f6818b6a57a431cf17fc4b9caaf5fc89
MaxAllen <maxton.allen@gmail.com>
2017-08-02 13:05:07 -0400

    lunch break

--- newtable.sql
+++ newtable.sql
@@ -50,6 +50,50 @@ FROM actors
 GROUP BY full_name
 ORDER BY rank DESC LIMIT 10;
 
+SELECT first_name || ' ' || last_name full_name, id, COUNT(*) rank
+FROM actors
+LEFT OUTER JOIN roles
+ON actors.id = roles.actor_id
+GROUP BY actors.id
+ORDER BY rank DESC LIMIT 10; 
+
+SELECT genre, COUNT(*) rank
+FROM movies_genres
+INNER JOIN movies
+ON movies_genres.movie_id = movies.id
+GROUP BY movies_genres.genre
+ORDER BY rank ASC;
+
+SELECT first_name, last_name, role, name
+FROM (SELECT first_name, last_name, role, movie_id
+	FROM actors
+	INNER JOIN roles
+	ON actors.id = roles.actor_id) joined
+INNER JOIN movies
+ON joined.movie_id = movies.id
+WHERE movies.name LIKE 'Braveheart'
+ORDER BY last_name ASC;
+
+SELECT movies.directors.director_id, movies.directors.movie_id,
+	(SELECT movies.id movie_id, movies.year, movies_genres.genre
+	FROM movies
+	INNER JOIN movies_genres 
+	ON movies.id = movies_genres.movie_id
+	WHERE movies.year%4 = 0
+	AND movies_genres.genre = 'Film-Noir') leapfilms
+	(SELECT directors.first_name,directors.last_name,movies_directors.movie_id
+	FROM directors
+	INNER JOIN movies_directors
+	ON director_id = id) directorMovies
+
+
+
+FROM directors
+INNER JOIN leapfilms
+ON 
+
+
+
 
 
 
