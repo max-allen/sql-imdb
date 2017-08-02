3db208bc677a8b5053d533663d16024cf5cd1a13
MaxAllen <maxton.allen@gmail.com>
2017-08-02 15:35:44 -0400

    finished bacon

--- newtable.sql
+++ newtable.sql
@@ -74,23 +74,54 @@ ON joined.movie_id = movies.id
 WHERE movies.name LIKE 'Braveheart'
 ORDER BY last_name ASC;
 
-SELECT movies.directors.director_id, movies.directors.movie_id,
-	(SELECT movies.id movie_id, movies.year, movies_genres.genre
+SELECT directorMovies.first_name || ' ' || directorMovies.last_name full_name,leapfilms.name movie_name, leapfilms.year year
+FROM (SELECT movies.id movie_id, movies.name, movies.year, movies_genres.genre
 	FROM movies
 	INNER JOIN movies_genres 
 	ON movies.id = movies_genres.movie_id
 	WHERE movies.year%4 = 0
 	AND movies_genres.genre = 'Film-Noir') leapfilms
-	(SELECT directors.first_name,directors.last_name,movies_directors.movie_id
+	
+INNER JOIN (SELECT directors.first_name,directors.last_name,movies_directors.movie_id 
 	FROM directors
 	INNER JOIN movies_directors
 	ON director_id = id) directorMovies
 
+ON directorMovies.movie_id = leapfilms.movie_id
+ORDER BY full_name ASC;
+
+SELECT actors.first_name || ' ' || actors.last_name full_name, drama_actors_films.name name
+FROM	
+	(SELECT actor_id, movie_id, movies.name name, role
+	FROM
+		(SELECT actor_id, roles.movie_id movie_id, role
+		FROM	
+			(SELECT bacon_films.movie_id
+			FROM
+				(SELECT DISTINCT movie_id, actor_id
+				FROM
+					(SELECT id, first_name || ' ' || last_name full_name 
+					FROM actors
+					WHERE full_name = 'Kevin Bacon') bacon_id
+				INNER JOIN roles
+				ON actor_id = id) bacon_films
+			INNER JOIN movies_genres
+			ON movies_genres.movie_id = bacon_films.movie_id
+			WHERE movies_genres.genre = 'Drama') drama_films
+		INNER JOIN roles
+		ON roles.movie_id = drama_films.movie_id
+		WHERE NOT roles.actor_id = 22591) drama_actors
+	INNER JOIN movies
+	ON movies.id = drama_actors.movie_id) drama_actors_films
+INNER JOIN actors
+ON actors.id = drama_actors_films.actor_id;
 
 
-FROM directors
-INNER JOIN leapfilms
-ON 
+
+
+
+
+
 
 
 
